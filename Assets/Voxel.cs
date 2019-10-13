0c95074ac07b07f1fbd8283fdbcd8fec698edbef
fhilprec <44507214+fhilprec@users.noreply.github.com>
2019-10-13 16:46:20 +0200

    a

--- Assets/Voxel.cs
+++ Assets/Voxel.cs
@@ -4,32 +4,118 @@ using UnityEngine;
 
 public class Voxel : MonoBehaviour
 {
-    public Vector3 size = new Vector3(10,10,10);
+    const int chuncksize = 10;
     Noise noise = new Noise();
+    [Range(0,1)]
+    public float threshhold;
+    TriAngulationTable tri = new TriAngulationTable();
+    [Range(0.1f,1)]
+    public float frequenzy;
+    float[,,] noisevalues = new float[chuncksize, chuncksize, chuncksize];
+    Mesh mesh;
+    GameObject cube;
+    float time = 1;
+    public Material mat;
+    private Vector3[] vertices;
+    private int[] triangles;
+
+    int x = 0;
+    int y = 0;
+    int z = 0;
+
+    float test = 0;
+
+    public float cubespeed = 1;
+
+
+    private void triangulate()
+    {
+        Mesh mesh = new Mesh();
+
+        List<Vector3> vertices = new List<Vector3>();
+        List<int> triangles = new List<int>();
+
+
+        Vector3[] verticesarray = new Vector3[3];
+        int[] trianglesarray = new int[3];
+
+
+        mesh.vertices = verticesarray;
+        mesh.triangles = trianglesarray;
+
+        gameObject.GetComponent<MeshFilter>().mesh = mesh;
+
+
+
+    }
+
     private void Start()
     {
-        //create the voxels
-        for(int x = 0; x  < size.x; x++)
-        {
-            for (int y = 0; y < size.y; y++)
-            {
-                for (int z = 0; z < size.z; z++)
-                {
-                    float v = noise.Evaluate(new Vector3(x, y, z));
+        
+        gameObject.AddComponent<MeshFilter>();
+        gameObject.AddComponent<MeshRenderer>();
+        gameObject.GetComponent<MeshRenderer>().sharedMaterial = mat;
+        //gameObject.GetComponent<MeshRenderer>().sharedMaterial.color = new Color(1, 0, 0);
 
-                    GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                    sphere.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
-                    sphere.transform.position = new Vector3(x, y, z);
-                    Color color = new Color(0, 0, v*100, v);
-                    sphere.GetComponent<MeshRenderer>().material.color = color;
+
+        //filling the 3d array with the noisevalues
+        for (int x = 0; x  < chuncksize; x++)
+        {
+            for (int y = 0; y < chuncksize; y++)
+            {
+                for (int z = 0; z < chuncksize; z++)
+                {
+                    float noisevalue = (noise.Evaluate(new Vector3(x, y, z) * frequenzy)+1)/2;
+                    noisevalues[x, y, z] = noisevalue;
+                    Debug.Log(noisevalue);
+                    if (noisevalue > threshhold)
+                    {
+                        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                        sphere.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+                        sphere.transform.position = new Vector3(x, y, z);
+                    }
+
                 }
+
+            }
+        }
+
+
+        //move the cube
+
+        cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+
+
+
+
+        //check whether  all the points of the cube are in the according surface
+        cube.transform.position = new Vector3(0.5f,0.5f, 0.5f);
+        
+
+    }
+
+    private void Update()
+    {
+
+        test += 0.01f;
+        Mesh mesh = new Mesh();
+        time += Time.deltaTime;
+        while (time > cubespeed)
+        {
+                if (x > chuncksize - 2) { x = 0; y++; }
+                if (y > chuncksize - 2) { x = 0; y = 0; z++; }
+                if (z > chuncksize - 2) { x = 0; y = 0; z = 0; }
+
+            cube.transform.position = new Vector3(x, y, z) + new Vector3(0.5f, 0.5f, 0.5f);
+            triangulate();
+
+
+
+            x++;
+                time = 0;
             }
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+
---
+++ Assets/scratch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scratch : MonoBehaviour
+{
+    public Material test;
+    private void Start()
+    {
+        int[] triangles = new int[3];
+        Vector3[] vertices = new Vector3[3];
+
+        vertices[0] = new Vector3(1, 0, 0);
+        vertices[1] = new Vector3(1, 1, 0);
+        vertices[2] = new Vector3(0, 1, 0);
+
+        triangles[0] = 0;
+        triangles[1] = 1;
+        triangles[2] = 2;
+
+        Mesh mesh = new Mesh();
+
+        gameObject.AddComponent<MeshFilter>();
+
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        gameObject.GetComponent<MeshFilter>().mesh = mesh;
+        gameObject.AddComponent<MeshRenderer>();
+        gameObject.GetComponent<MeshRenderer>().sharedMaterial = test;
+
+    }
+}
